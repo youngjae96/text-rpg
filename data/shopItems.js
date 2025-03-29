a5e132500dff7399cc88c8f9abd97e6d186aa5fc
youngjae96 <thrrltk321@gmail.com>
2025-03-30 00:17:03 +0900

    Update shopItems.js

--- data/shopItems.js
+++ data/shopItems.js
@@ -1,7 +1,7 @@
 module.exports = [
-  { name: '나무검', type: 'weapon', str: 3, price: 50, grade: '일반' },
-  { name: '청동검', type: 'weapon', str: 6, price: 100, grade: '고급' },
-  { name: '모험가 갑옷', type: 'armor', vit: 2, price: 80, grade: '일반' },
-  { name: '마나링', type: 'accessory', wis: 2, mp: 10, price: 70, grade: '희귀' },
+  { name: '나무검', type: 'weapon', stats: { attack: 3, defense: 0, hp: 0, mp: 0 }, price: 50, grade: '일반' },
+  { name: '청동검', type: 'weapon', stats: { attack: 6, defense: 0, hp: 0, mp: 0 }, price: 100, grade: '고급' },
+  { name: '모험가 갑옷', type: 'armor', stats: { attack: 0, defense: 2, hp: 0, mp: 0 }, price: 80, grade: '일반' },
+  { name: '마나링', type: 'accessory', stats: { attack: 0, defense: 0, hp: 0, mp: 10 }, price: 70, grade: '희귀' },
   { name: 'HP 포션', type: 'consumable', effect: 'heal', price: 30, grade: '일반' }
 ];
