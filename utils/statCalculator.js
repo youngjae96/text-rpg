2989c50eb768f5d0f3b94440e0ebd3dd7835edd4
youngjae96 <thrrltk321@gmail.com>
2025-03-30 00:19:41 +0900

    Update statCalculator.js

--- utils/statCalculator.js
+++ utils/statCalculator.js
@@ -1,23 +1,25 @@
 // utils/statCalculator.js
+
 const { getItemStatsByName } = require('./itemStats');
 
 function calculateTotalStats(user) {
+  // 기본 스탯 설정
   const baseStats = {
-    str: user.str,
-    dex: user.dex,
-    int: user.int,
-    vit: user.vit,
-    wis: user.wis,
-    luk: user.luk
+    attack: user.attack || 0,
+    defense: user.defense || 0,
+    hp: user.hp || 0,
+    mp: user.mp || 0
   };
 
+  // 장비 아이템의 스탯을 합산
   for (let type of ['weapon', 'armor', 'accessory']) {
     const item = user.equipped[type];
     if (item && item.name) {
       const bonus = getItemStatsByName(item.name);
-      for (let key in bonus) {
-        baseStats[key] += bonus[key];
-      }
+      baseStats.attack += bonus.attack || 0;
+      baseStats.defense += bonus.defense || 0;
+      baseStats.hp += bonus.hp || 0;
+      baseStats.mp += bonus.mp || 0;
     }
   }
 
